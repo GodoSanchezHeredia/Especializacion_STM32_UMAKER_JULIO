5a2c54cecd22930c130c68722dfe0fa2558dd809
GodoSanchezHeredia <godo.electronica@gmail.com>
2026-08-06 20:12:10 -0500

    add intx tx uart

--- Ejemplo7_USART/Core/Src/main.c
+++ Ejemplo7_USART/Core/Src/main.c
@@ -17,8 +17,20 @@ volatile uint32_t i = 0;
 USART_Handle_t husart2;
 
 uint8_t mensaje[] = "caracter invalido\n\r";
+uint8_t mensaje1[] =
+        "\r\n"
+        "========================================\r\n"
+        " STM32 - TRANSMISION POR INTERRUPCION\r\n"
+        "========================================\r\n"
+        "El boton PC13 ha sido presionado.\r\n"
+        "Este mensaje se envia byte por byte\r\n"
+        "utilizando las interrupciones TXE y TC.\r\n"
+        "El CPU puede seguir ejecutando el main.\r\n"
+        "========================================\r\n\r\n";
 
 volatile uint8_t rxByte = 0;
+uint8_t botonActual;
+uint8_t botonAnterior = 1;
 
 void USART_GPIO_INIT(void);
 
@@ -29,6 +41,7 @@ int main(void){
 	 USART_GPIO_INIT();
 	 USART_Init(USART2, 42E+6, 115200, 0);
 	 Gpio_Init(PORT_A, 5,OUTPUT	, OUT2, NO_PULLUP_NO_PULL_DOWN);
+	 Gpio_Init(PORT_C, 13, INPUT, OUT2, NO_PULLUP_NO_PULL_DOWN);
 	 // USART_SendData(USART2, &mensaje, sizeof(mensaje)-1U);
 	 //USART_SendData(USART2, &dato, 1);
 
@@ -43,6 +56,23 @@ int main(void){
 	for (;;) {
 
 
+        botonActual = (GPIOC->IDR & (1U << 13)) ? 1U : 0U;
+
+        if((botonAnterior == 1U) &&
+           (botonActual == 0U))
+        {
+
+            if(husart2.TxBusyState == USART_READY)
+            {
+
+                USART_SendDataIT(&husart2,mensaje1,sizeof(mensaje1) - 1U);
+            }
+
+        }
+
+
+        /* Actualizar estado anterior */
+        botonAnterior = botonActual;
 
 
 	}
@@ -73,6 +103,16 @@ void USART_ApplicationEventCallback(USART_Handle_t *pUSARTHandle,uint8_t event){
 		 USART_ReceiveDataIT(&husart2, &rxByte, 1U);
 
 	}
+	if(pUSARTHandle->pUSARTx == USART2)
+	    {
+
+
+	        if(event == USART_EVENT_TX_CMPLT)
+	        {
+	        	Gpio_Write(PORT_A, 5, 1);
+	        }
+
+	    }
 
 }
 
